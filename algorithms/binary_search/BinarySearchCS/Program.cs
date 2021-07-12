ddb9621149a81341c05c960d71fde913908f29bf
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-07-12 14:51:30 +0200

    Add binary search solutions

---
+++ algorithms/binary_search/BinarySearchCS/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BinarySearchCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine($"Table is {string.Join(", ", list)}");
+            Console.WriteLine("What number are you looking for: ");
+            var search = int.Parse(Console.ReadLine());
+            var result = BinarySearch(list, search);
+            Console.WriteLine($"Binary search result is {result}");
+            Console.WriteLine($"Number under index of {result} is {list[result]}");
+        }
+
+        static int BinarySearch(int[] list, int item)
+        {
+            int first = 0;
+            int last = list.Length - 1;
+            while (first <= last)
+            {
+                var middle = (first + last) / 2;
+                int guess = list[middle];
+                if (guess == item)
+                {
+                    return middle;
+                }
+                if (guess > item)
+                { // item ∈ (first;guess)
+                    last = guess - 1;
+                }
+                else if (guess < item)
+                { // item ∈ (guess;last)
+                    first = guess + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ algorithms/binary_search/binary_search.py
@@ -0,0 +1,20 @@
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def binary_search(list, item):
+    low = 0  # Pierwszy element tabeli
+    high = len(list) - 1  # Ostatni element tabeli
+
+    while low <= high:  # Dopóki nie pozostanie jeden element
+        mid = (low + high) // 2  # Wybieramy środkowy element
+        guess = list[mid]  # Wybieramy środkowy element tabeli
+        if guess == item:  # Znaleziono element
+            return mid
+        if guess > item:  # Guess był większy niż poszukiwana liczba
+            high = mid - 1
+        elif guess < item:  # Guess był mniejszy niż poszukiwana liczba
+            low = mid + 1
+    return None  # Nie znaleziono
+
+
+print(binary_search(items, 8))
